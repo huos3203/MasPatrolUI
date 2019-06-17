eb901b915d9d05bacdd1168650114f9dc4661c80
likuan <377509050@qq.com>
2019-06-17 20:15:13 +0800

    屏蔽多余功能

--- MasPatrolUI/DrawEditor/DrawEditerController.m
+++ MasPatrolUI/DrawEditor/DrawEditerController.m
@@ -60,12 +60,8 @@
     
     CLImageToolInfo *tool0 = [editor.toolInfo subToolInfoWithToolName:@"CLDrawTool" recursive:NO];
     tool0.title = @"涂鸦";
-    tool0.available = YES;//如果available设置为no，则从菜单视图中删除。
-    tool0.dockedNumber = -1;//置于顶层
     CLImageToolInfo *tool01 = [editor.toolInfo subToolInfoWithToolName:@"CLTextTool" recursive:NO];
     tool01.title = @"文字";
-    tool01.available = YES;//如果available设置为no，则从菜单视图中删除。
-    [self removeOtherBut:editor];
     
     [self presentViewController:editor animated:YES completion:nil];
     //[editor showInViewController:self withImageView:_imageView];
@@ -80,42 +76,6 @@
         PatrolOptionSelController *optselVC = [PatrolOptionSelController new];
         [self presentViewController:optselVC animated:YES completion:nil];
     }];
-}
-
-
--(void)removeOtherBut:(CLImageEditor *)editor
-{
-    CLImageToolInfo *tool = [editor.toolInfo subToolInfoWithToolName:@"CLToneCurveTool" recursive:NO];
-    tool.available = NO;
-    
-    CLImageToolInfo *tool1 = [editor.toolInfo subToolInfoWithToolName:@"CLRotateTool" recursive:YES];
-    tool1.available = NO;
-    
-    CLImageToolInfo *tool2 = [editor.toolInfo subToolInfoWithToolName:@"CLHueEffect" recursive:YES];
-    tool2.available = NO;
-    
-    CLImageToolInfo *tool3 = [editor.toolInfo subToolInfoWithToolName:@"CLBlurTool" recursive:YES];
-    tool3.available = NO;
-    CLImageToolInfo *tool4 = [editor.toolInfo subToolInfoWithToolName:@"CLAdjustmentTool" recursive:YES];
-    tool4.available = NO;
-    CLImageToolInfo *tool5 = [editor.toolInfo subToolInfoWithToolName:@"CLEffectTool" recursive:YES];
-    tool5.available = NO;
-    //滤镜
-    CLImageToolInfo *tool6 = [editor.toolInfo subToolInfoWithToolName:@"CLFilterTool" recursive:YES];
-    tool6.available = NO;
-    CLImageToolInfo *tool7 = [editor.toolInfo subToolInfoWithToolName:@"CLSplashTool" recursive:YES];
-    tool7.available = NO;
-    //贴图
-    CLImageToolInfo *tool8 = [editor.toolInfo subToolInfoWithToolName:@"CLEmoticonTool" recursive:YES];
-    tool8.available = NO;
-    CLImageToolInfo *tool9 = [editor.toolInfo subToolInfoWithToolName:@"CLStickerTool" recursive:YES];
-    tool9.available = NO;
-    //放大缩小
-    CLImageToolInfo *tool10 = [editor.toolInfo subToolInfoWithToolName:@"CLResizeTool" recursive:YES];
-    tool10.available = NO;
-    //剪切
-    CLImageToolInfo *tool11 = [editor.toolInfo subToolInfoWithToolName:@"CLClippingTool" recursive:YES];
-    tool11.available = NO;
 }
 
 @end
